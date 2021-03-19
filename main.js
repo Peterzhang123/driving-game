e3a5e01e29cf6b3e4288c86f5d9acfc8a79b28f4
Peterzhang123 <peterzzq1986@gmail.com>
2021-03-19 14:42:51 -0700

    user can turn the car with degree=0 method

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ var left = 37;
 var up = 38;
 var right = 39;
 var down = 40;
-var degree = 90;
+var degree = 0;
 var img;
 var transition = "300ms ease all";
 
@@ -49,31 +49,134 @@ var lastKeyDown = function (prevKey, currKey) {
 }
 
 function upArrow(prevKey) {
-      img.css({
+  if (prevKey == left) {
+    degree += 90;
+    img.css({
       transform: `rotate(${degree}deg)`,
       transition: transition,
     });
+  } else if (prevKey == right) {
+    degree -= 90;
+    img.css({
+      transform: `rotate(${degree}deg)`,
+      transition: transition,
+    });
+  } else if (prevKey == down) {
+    degree -= 180;
+    img.css({
+      transform: `rotate(${degree}deg)`,
+      transition: transition,
+    });
+  } else if (prevKey == 0) {
+    degree -= 90;
+    img.css({
+      transform: `rotate(${degree}deg)`,
+      transition: transition,
+    })
+  }else{
+    degree;
+    img.css({
+      transform: `rotate(${degree}deg)`,
+      transition: transition,
+    })
+  }
 }
 
 function downArrow(prevKey) {
+  if (prevKey == right) {
+    degree+=90;
     img.css({
-      transform: `rotate(${degree * 3}deg)`,
+      transform: `rotate(${degree}deg)`,
       transition: transition,
     });
+  } else if (prevKey == left) {
+    degree-=90;
+      img.css({
+        transform: `rotate(${degree}deg)`,
+        transition: transition,
+      });
+  } else if (prevKey == up) {
+    degree+=180;
+    img.css({
+      transform: `rotate(${degree}deg)`,
+      transition: transition,
+    });
+  } else if (prevKey == 0) {
+    degree+=90;
+    img.css({
+      transform: `rotate(${degree}deg)`,
+      transition: transition,
+    })
+  } else {
+    degree;
+    img.css({
+      transition: transition,
+      transform: `rotate(${degree}deg)`,
+    });
+  }
 }
 
 function leftArrow(prevKey) {
+  if (prevKey == right) {
+    degree+=180;
     img.css({
-      transform: `rotate(0deg)`,
+      transform: `rotate(${degree}deg)`,
       transition: transition,
-    });   
+    })
+  } else if (prevKey == up) {
+    degree-=90;
+    img.css({
+      transform: `rotate(${degree}deg)`,
+      transition: transition,
+    })
+  } else if (prevKey == down) {
+    degree+=90;
+    img.css({
+      transform: `rotate(${degree}deg)`,
+      transition: transition,
+    })
+  } else if (prevKey == 0) {
+    degree+=180;
+    img.css({
+      transform: `rotate(${degree}deg)`,
+      transition: transition,
+    })
+  } else {
+    img.css({
+      transition: transition,
+    });
+  }
 }
 
 function rightArrow(prevKey) {
+  if (prevKey == left) {
+    degree+=180;
+    img.css({
+      transform: `rotate(${degree}deg)`,
+      transition: transition,
+    });
+  } else if (prevKey == up) {
+    degree+=90;
+    img.css({
+      transform: `rotate(${degree}deg)`,
+      transition: transition,
+    });
+  } else if (prevKey == down) {
+    degree -= 90;
+    img.css({
+      transform: `rotate(${degree}deg)`,
+      transition: transition,
+    });
+  } else if (prevKey == right || prevKey == 0) {
+    degree;
     img.css({
       transition: transition,
-      transform: `rotate(${degree * 2}deg)`,
+      transform: `rotate(${degree}deg)`,
     });
+  }
 
 }
+
+
+
 
